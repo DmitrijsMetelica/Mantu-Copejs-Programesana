73733455dad59d85a1039d5fd7499087ad1dd0be
DmitrijsMetelica <dmitrijs.metelica@marupe.edu.lv>
2024-03-25 23:01:25 +0200

    ***** ......

--- db.php
+++ db.php
@@ -1,16 +1,19 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mantu_copejs";
 
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "mantu_copejs";
+// Izveido savienojumu ar datu bāzi
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// $conn = mysqli_connect($servername, $username, $password, $dbname);
+// Pārbauda savienojuma veiksmīgumu
+if (!$conn) {
+    die("Savienojuma neizdevās: " . mysqli_connect_error());
+} else {
+    echo "Savienojums veiksmīgs";
+}
 
-// if(!$conn){
-//     die("Savienojuma neizdevās: " . mysqli_connect_error());
-// }
-// else{
-//     echo "Savienojums veiksmīgs";
-// }
+// Norādām, ka izmantojam UTF-8 kodējumu
+mysqli_set_charset($conn, "utf8");
 ?>
--- register.php
+++ register.php
@@ -1,12 +1,23 @@
 <?php
-//  require_once('db.php');
+// Iekļaujam datu bāzes savienojumu
+require_once('db_connection.php');
 
-echo $_POST['reguname'];
-// $usname = $_POST['regusname'];
-// $email = $_POST['regemail'];
-// $password = $_POST['regpassword'];
+// Saņemam datus no formas
+$uname = $_POST['reguname'];
+$usname = $_POST['regusname'];
+$email = $_POST['regemail'];
+$password = $_POST['regpassword'];
 
-// $sql = "INSERT INTO user_datubase (uname, usname, email, password) VALUES ('$reguname', '$regusname', '$regemail', '$regpassword')";
+// Izveidojam SQL vaicājumu, pielāgojot to datu bāzes nosaukumam
+$sql = "INSERT INTO user_datubase (uname, usname, email, password) VALUES ('$uname', '$usname', '$email', '$password')";
 
-// $conn -> query($sql);
-// ?>
+// Palaist SQL vaicājumu un pārbaudīt rezultātu
+if ($conn->query($sql) === TRUE) {
+    echo "Jauns ieraksts veiksmīgi pievienots";
+} else {
+    echo "Kļūda: " . $sql . "<br>" . $conn->error;
+}
+
+// Aizveram savienojumu ar datu bāzi
+$conn->close();
+?>

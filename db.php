c659f5cbed19b83b1ff32384c1f22cf815072032
DmitrijsMetelica <dmitrijs.metelica@marupe.edu.lv>
2024-03-25 22:27:08 +0200

    fucking error

--- db.php
+++ db.php
@@ -1,6 +1,16 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "user_datubase"
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "mantu_copejs";
+
+// $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// if(!$conn){
+//     die("Savienojuma neizdevās: " . mysqli_connect_error());
+// }
+// else{
+//     echo "Savienojums veiksmīgs";
+// }
+?>
--- register.php
+++ register.php
@@ -1,5 +1,12 @@
 <?php
-$uname = $_POST['uname'];
-$usname = $_POST['usname'];
-$email = $_POST['email'];
-$password = $_POST['password'];
+//  require_once('db.php');
+
+echo $_POST['reguname'];
+// $usname = $_POST['regusname'];
+// $email = $_POST['regemail'];
+// $password = $_POST['regpassword'];
+
+// $sql = "INSERT INTO user_datubase (uname, usname, email, password) VALUES ('$reguname', '$regusname', '$regemail', '$regpassword')";
+
+// $conn -> query($sql);
+// ?>
